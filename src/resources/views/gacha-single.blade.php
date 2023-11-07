b3af077d657531050b199e1b5f47127a0384ec74
UmiAzuki <asuwandamu@gmail.com>
2023-11-08 00:42:39 +0900

    表示方法模索

--- src/resources/views/gacha-single.blade.php
+++ src/resources/views/gacha-single.blade.php
@@ -8,7 +8,7 @@ const RARITYFORIMAGE = ['', 'normal', 'normalplus', 'rare', 'rareplus', 'srare']
 <body class="w-100 h-100 text-white bg-dark container">
     <div class="cover-container">
         <?php
-        $card = 'gachaResult';
+        $card = $gachaResult;
         ?>
         <main class="px-3">
             <canvas id="gachaDisplay"></canvas>
